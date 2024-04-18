5b8da2e6fd68058b04ceec53da3a0b17b589b655
ominivac <robertokbs@gmail.com>
2024-04-18 09:37:35 -0300

    criação do projeto

---
+++ src/main/java/com/irede/CatalogoIredeApplication.java
@@ -0,0 +1,13 @@
+package com.irede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoIredeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoIredeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irede/CatalogoIredeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoIredeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
