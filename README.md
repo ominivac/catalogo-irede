cd6a1ff84b1d2d1e2e5b7ea5b0b32a36186bae70
ominivac <robertokbs@gmail.com>
2024-04-22 09:23:28 -0300

    correcao sqls e application.properties habilitar ou nao o docker

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 #### Repositório para teste da IREDE
 
 Link para o swagger localhost :		<http://localhost:8080/swagger-ui/index.html>
+
+observações :
+
+- Importar a app para o diretório local
+- Se preferir rodar sem o docker por 'spring.docker.compose.enabled=false' no application.properties, true para subir pelo docker
+- em /src/main/resource há arquivos json e sql para importar os dados se preferir
+- o docker não gera o schema automático criar usando : create_database.sql em /resources
+
---
+++ src/main/resources/INSERT_SALAS.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM catalogo_db.sala
+LIMIT 0, 1000
+
+-- Date: 2024-04-22 09:21
+*/
+INSERT INTO `` (`id`,`descricao`,`numero_sala`) VALUES (1,'descrição sala 01','sala 01');
+INSERT INTO `` (`id`,`descricao`,`numero_sala`) VALUES (2,'descrição sala 02','sala 02');
+INSERT INTO `` (`id`,`descricao`,`numero_sala`) VALUES (3,'descrição sala 03','sala 03');
+INSERT INTO `` (`id`,`descricao`,`numero_sala`) VALUES (4,'descrição sala 04','sala 04');
+INSERT INTO `` (`id`,`descricao`,`numero_sala`) VALUES (5,'descrição sala 05','sala 05');
